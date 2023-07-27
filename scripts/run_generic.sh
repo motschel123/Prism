f5a37403b420f4da03e7367dcf2bf208b3346846
Thomas Kob <uk16ural@aiss-cleary.aibe.uni-erlangen.de>
2023-07-27 15:02:40 +0200

    Insert parameters + fixes

--- scripts/run_generic.sh
+++ scripts/run_generic.sh
@@ -2,19 +2,27 @@ export NEPTUNE_TOKEN=
 export NEPTUNE_PROJECT=
 conda activate prism
 
-DATE_TIME=$(date +%Y-%m-%d_%H_%M)
-OUTPUT_FILE=/data/$USER/prism_log/run_$DATE_TIME.txt
-PARAMS=("1 2 3" "4 5 6") # Insert parameters here
+# Arguments for runs
+# Usage: rigid <n rigid phases> <rigid cov> <transition cov> <name>
+# Usage: normal <name>
+PARAMS=("normal no_rigid_phases" "rigid 30 0.001 0.0001 many_tiny_stops" "rigid 5 0.02 0.005 some_short_stops" "rigid 3 0.05 0.01 some_rigid_phases")
 
 # Make sure neptune data is stored locally and not in the RRZE network 
 mkdir -p /data/$USER/neptune
-ln -s /data/$USER/neptune
+mkdir -p /data/$USER/prism_log
+ln -s /data/$USER/neptune .neptune
 
 echo PRISM > $FILE
 
 for i in "${PARAMS[@]}"
-DATE_TIME=$(date +%Y-%m-%d_%H_%M)
-OUTPUT_FILE=/data/$USER/prism_log/run_$DATE_TIME.txt
 do
+    DATE_TIME=$(date +%Y-%m-%d_%H_%M)
+    OUTPUT_FILE=/data/$USER/prism_log/run_$DATE_TIME.txt
+    echo Performing run for parameters "$i"...
+    echo Log is stored in $OUTPUT_FILE
+
+    # Start run
     python -m workstation/run_dustin_generic.py $i > $OUTPUT_FILE 2>&1
 done
+
+echo Done!
